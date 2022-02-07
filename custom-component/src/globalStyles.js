42b8b7d779a64fe1a6ffead0d5f7c37faf455f1c
y-solb <ysolb99@gmail.com>
2022-02-07 17:33:59 +0900

    feat: globalStyles 추가

---
+++ custom-component/src/globalStyles.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        font-family:'Arial',sans-serif ;
+    }
+
+    body{
+        box-sizing:border-box;
+    }
+
+    button{
+        border:none;
+        outline: none;
+        font-size: 14px;
+    }
+
+    ul{
+        list-style: none;
+    }
+`;
+
+export default GlobalStyle;
