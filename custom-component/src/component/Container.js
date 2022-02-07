d3bdfdeea3815e772ab36bf8560b056788575d64
y-solb <ysolb99@gmail.com>
2022-02-07 17:35:23 +0900

    feat: component를 감싸는 Container component 추가

---
+++ custom-component/src/component/Container.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = (props) => {
+  const { title, children } = props;
+
+  return (
+    <ComponentContainer>
+      <h2>{title}</h2>
+      <ComponentWrapper>{children}</ComponentWrapper>
+    </ComponentContainer>
+  );
+};
+
+export default Container;
+
+const ComponentContainer = styled.div`
+  margin: 40px 0;
+`;
+
+const ComponentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 40px 0;
+`;
