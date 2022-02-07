546b30f6bbe007f822478c81a3ac67f26933a842
y-solb <ysolb99@gmail.com>
2022-02-07 17:37:47 +0900

    feat: App 구현

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -1,7 +1,34 @@
-import "./App.css";
+import GlobalStyle from "./globalStyles";
+import styled from "styled-components";
+import Toggle from "./component/Toggle";
+import Modal from "./component/Modal";
+import Tab from "./component/Tab";
+import Tag from "./component/Tag";
+import AutoComplete from "./component/AutoComplete";
+import ClickToEdit from "./component/ClickEdit";
 
 function App() {
-  return <h2>wanted_pre_onboarding</h2>;
+  return (
+    <>
+      <GlobalStyle />
+      <Layout>
+        <Toggle />
+        <Modal />
+        <Tab />
+        <Tag />
+        <AutoComplete />
+        <ClickToEdit />
+      </Layout>
+    </>
+  );
 }
 
 export default App;
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1140px;
+  margin: auto;
+  padding: 0 30px;
+`;
